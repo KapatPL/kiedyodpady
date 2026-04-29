0e38ef97ee1494a4430fadd5a3cc69f8f4ca9adc
Tomasz Kapusciarek <tomasz@kapusciarek.pl>
2026-04-30 01:09:49 +0200

    Add const.py

---
+++ const.py
@@ -0,0 +1,22 @@
+DOMAIN = "kiedyodpady"
+PLATFORM = "sensor"
+
+CONF_LOCALITY_ID = "locality_id"
+CONF_STREET_ID = "street_id"
+CONF_NUMBER = "number"
+CONF_PROPERTY_TYPE = "property_type"
+CONF_BUILDING_TYPE = "building_type"
+CONF_ORIGIN = "origin"
+
+DEFAULT_ORIGIN = "https://jozefow.kiedyodpady.pl"
+DEFAULT_PROPERTY_TYPE = "Zamieszkana"
+DEFAULT_BUILDING_TYPE = "Jednorodzinna"
+
+WASTE_TYPES = {
+    "01887640-c4e2-4ee4-a9eb-f15ac6ed1a82": "Bio",
+    "01887642-b506-4f67-9db1-c14ff973d3a9": "Zielone",
+    "01887640-9754-4439-b1f2-34cd3cbf2b6c": "Zmieszane",
+    "01887643-ef73-43d7-81bd-fe8ac69e3721": "Metale i tworzywa sztuczne",
+    "01887643-58ca-4988-a2d9-153ff5b76a2a": "Papier",
+    "01887643-ae5a-4501-8ce7-207187937c58": "Szkło",
+}
